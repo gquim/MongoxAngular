69c5a23d42d20a1aa7baea187d708b703c397273
Isaac Caliz <isaaccaliz305@gmail.com>
2025-08-20 22:42:50 -0600

    insert and get user

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { UsuarioService } from './usuario.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  providers: [UsuarioService]
 })
-export class App {
-  protected readonly title = signal('angularMongo');
+export class AppComponent {
+  usuarios: any[] = [];
+  nuevoUsuario = { nombre: '', email: '', edad: null };
+
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit() {
+    this.cargarUsuarios();
+  }
+
+  cargarUsuarios() {
+    this.usuarioService.getUsuarios().subscribe(data => {
+      this.usuarios = data;
+    });
+  }
+
+  agregarUsuario() {
+    if (!this.nuevoUsuario.nombre || !this.nuevoUsuario.email) return;
+
+    this.usuarioService.crearUsuario(this.nuevoUsuario).subscribe(usuario => {
+      this.usuarios.push(usuario);
+      this.nuevoUsuario = { nombre: '', email: '', edad: null };
+    });
+  }
+
+  eliminarUsuario(id: string) {
+    this.usuarioService.eliminarUsuario(id).subscribe(() => {
+      this.usuarios = this.usuarios.filter(u => u._id !== id);
+    });
+  }
 }
